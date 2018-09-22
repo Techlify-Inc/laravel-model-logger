103266580eb181160d5b667bab17c2e3bf4aaad5
Techlify Inc <info@techlify.com>
2018-09-22 19:34:02 -0400

    Added some extra config options

--- src/Traits/LoggableModel.php
+++ src/Traits/LoggableModel.php
@@ -15,22 +15,35 @@ use TechlifyInc\LaravelModelLogger\Models\LoggerGuy;
 trait LoggableModel
 {
 
+    protected $logCreated = true;
+    protected $logUpdated = true;
+    protected $logDeleted = true;
+
     public static function boot()
     {
         parent::boot();
 
         static::created(function(Model $model) {
+            if (!$model->logCreated) {
+                return;
+            }
             $reflect = new ReflectionClass($model);
             LoggerGuy::logInserted($reflect->getShortName(), $model->id, $model);
             Log::debug($reflect->getShortName() . '.created');
         });
 
         static::updated(function(Model $model) {
+            if (!$model->logUpdated) {
+                return;
+            }
             $reflect = new ReflectionClass($model);
             Log::debug($reflect->getShortName() . '.updated');
         });
 
         static::deleted(function(Model $model) {
+            if (!$model->logDeleted) {
+                return;
+            }
             $reflect = new ReflectionClass($model);
             Log::debug($reflect->getShortName() . '.deleted');
         });
